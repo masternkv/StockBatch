12b33caa6f29de8a292325ba84ca20b88d6574fc
masternkv <niranjverma1063@gmail.com>
2021-03-07 10:14:07 +0530

    Added Price Change

--- src/main/java/com/stock/batch/listener/JobCompletionNotificationListener.java
+++ src/main/java/com/stock/batch/listener/JobCompletionNotificationListener.java
@@ -4,8 +4,10 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
@@ -41,12 +43,11 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
 
 	@Override
 	public void afterJob(JobExecution jobExecution) {
-		if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
-			
+		if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
 
 			StringBuffer sbf = new StringBuffer();
 
-		 			BufferedWriter bwr = null;
+			BufferedWriter bwr = null;
 			try {
 				bwr = new BufferedWriter(
 						new FileWriter(new File("C:\\Users\\NV5053984\\Desktop\\Stock List\\out.csv")));
@@ -54,7 +55,7 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 			List<NiftyStockCount> results = jdbcTemplate.query(
 					"SELECT id,stock_name, count FROM daily_nifty_count where count >=2",
 					new RowMapper<NiftyStockCount>() {
@@ -66,25 +67,33 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
 
 			for (NiftyStockCount niftycount : results) {
 				List<NiftyStock> niftydailyresult = jdbcTemplate.query(
-						"SELECT id,stock_name,stock_price,stock_chg_date FROM daily_nifty where stock_name='"+niftycount.getStockName()+"'order by stock_chg_date desc",
+						"SELECT id,stock_name,stock_price,stock_chg_date FROM daily_nifty where stock_name='"
+								+ niftycount.getStockName() + "'order by stock_chg_date desc",
 						new RowMapper<NiftyStock>() {
 							@Override
 							public NiftyStock mapRow(ResultSet rs, int row) throws SQLException {
-								return new NiftyStock(rs.getInt(1), rs.getString(2), rs.getString(3),rs.getString(4));
+								return new NiftyStock(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
 							}
 						});
 				sbf.append(niftycount.getStockName()).append(",");
+				String past_price = niftydailyresult.get(0).getStockPrice();
+				String current_price = niftydailyresult.get(niftydailyresult.size() - 1).getStockPrice();
+				List<Double> getComaprePrice = getFormattedNumber(past_price, current_price);
+
+				// Float change_price=(fist_price-current_price);
+				BigDecimal pastPrice = new BigDecimal(getComaprePrice.get(0));
+				BigDecimal curPrice = new BigDecimal(getComaprePrice.get(getComaprePrice.size() - 1));
+				BigDecimal result = pastPrice.subtract(curPrice);
+				sbf.append(result).append(",");
 				for (NiftyStock niftystock : niftydailyresult) {
 					{
-						String decimalValuePrint="\""+niftystock.getStockPrice()+"\"";
+						String decimalValuePrint = "\"" + niftystock.getStockPrice() + "\"";
 						sbf.append(niftystock.getStockChangeDate()).append(",").append(decimalValuePrint).append(",");
 					}
 				}
-				
+
 				sbf.append(System.getProperty("line.separator"));
 			}
-
-
 
 			// write contents of StringBuffer to a file
 			try {
@@ -115,5 +124,23 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
 		}
 	}
 
+	public List<Double> getFormattedNumber(String pastPrice, String currentPrice) {
 
+		List<Double> addPrice = new ArrayList<>();
+		Double storePast = 0.0;
+		Double storeCurrentprice = 0.0;
+
+		if (!pastPrice.isEmpty()) {
+			pastPrice = pastPrice.replaceAll(",", "");
+			storePast = Double.parseDouble(pastPrice);
+		}
+		if (!currentPrice.isEmpty()) {
+			currentPrice = currentPrice.replaceAll(",", "");
+			storeCurrentprice = Double.parseDouble(currentPrice);
+		}
+		addPrice.add(storePast);
+		addPrice.add(storeCurrentprice);
+
+		return addPrice;
+	}
 }
